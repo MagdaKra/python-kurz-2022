146de9a6cd44eb7175d3b7903e006acddd4c1dce
MagdaKra <100540725+MagdaKra@users.noreply.github.com>
2022-03-20 21:39:42 +0100

    Add files via upload

--- ukol-01.py
+++ ukol-01.py
@@ -6,6 +6,16 @@ baliky = {
     "B947X": False,
 }
 
+<<<<<<< HEAD
+code = input("Zadej kód balíku: ")
+
+if code in baliky:
+  if baliky[code] == True:
+    print(f"Balík {code} byl předán kurýrovi.")
+  else:
+    print("Balík zatím nebyl předán kurýrovi")
+    
+=======
 number = input("Zadej kód balíku: ")
 handed = number[True]
 for item in baliky:
@@ -14,3 +24,4 @@ for item in baliky:
     print(f"Balík číslo {handed} byl předán kurýrovi.")
   else:
     print("Balík zatím nebyl předán kurýrovi")
+>>>>>>> 8bbc6a8fa3a0f4013f60f9bfb95291a866bee57c
